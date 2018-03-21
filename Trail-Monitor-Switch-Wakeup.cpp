8474bb7e704b0640da6162362132cda3b5558751
Daniel <36209256+kell1426@users.noreply.github.com>
2018-03-21 18:28:05 -0500

    New Switch statement code with wake on pin being high from external battery.

---
+++ Trail-Monitor-Switch-Wakeup.cpp
@@ -0,0 +1,151 @@
+// This #include statement was automatically added by the Particle IDE.
+#include <AssetTracker.h>
+
+// This #include statement was automatically added by the Particle IDE.
+#include <SdFat.h>
+
+// Pick an SPI configuration.
+// See SPI configuration section below (comments are for photon).
+#define SPI_CONFIGURATION 0
+//------------------------------------------------------------------------------
+// Setup SPI configuration.
+#if SPI_CONFIGURATION == 0
+SdFatSoftSpi<D3, D2, D4> sd;
+const uint8_t chipSelect = D5;
+#endif  // SPI_CONFIGURATION
+//------------------------------------------------------------------------------
+#define DEBUG 0
+SYSTEM_MODE(SEMI_AUTOMATIC);
+int id = 100;
+File myFile;
+AssetTracker t = AssetTracker();
+long lastGPSpoint = 0;
+uint32_t initialTime = 0;
+uint32_t offset = 0;
+enum State { WAKE_STATE, ACQUIRE_STATE, SEND_STATE, POWER_DOWN_STATE};
+State state = WAKE_STATE;
+int pin;
+int count = 0;
+long lastGPSpoint = 0;
+
+void sendStateChange();
+
+void setup()
+{
+  Serial.begin(9600);
+  // Wait for USB Serial
+  while (!Serial)
+  {
+  SysCall::yield();
+  }
+
+  if (!sd.begin(chipSelect, SPI_HALF_SPEED))
+  {
+    sd.initErrorHalt();
+  }
+
+  myFile.open("TrailData.txt", O_RDWR | O_CREAT);
+  myFile.close();
+
+  t.begin();	//Set up Asset Tracker bits
+
+  pinMode(D1, INPUT_PULLDOWN);
+
+  attachInterrupt(D1, sendStateChange, FALLING);
+  delay(5000);
+}
+
+void loop()
+{
+  switch(state)
+  {
+    case WAKE_STATE:
+    Serial.println("In WAKE_STATE");
+      t.gpsOn();
+      if(DEBUG == 1)
+      {
+        state = ACQUIRE_STATE;
+        Serial.println("Moving to ACQUIRE_STATE");
+        break;
+      }
+      Cellular.on();
+      Cellular.connect();
+      while(!Cellular.ready());
+      Particle.connect();
+      delay(5000);
+      initialTime = Time.local();
+      offset = millis();
+      Particle.disconnect();
+      Cellular.disconnect();
+      Cellular.off();
+      state = ACQUIRE_STATE;
+      break;
+    case ACQUIRE_STATE:
+      //Serial.println("In ACQUIRE_STATE");
+      //delay(1000);
+      t.updateGPS();
+      if ((millis()-lastGPSpoint) > (1000))
+      {
+        lastGPSpoint = millis();
+        if(t.gpsFix())
+        {
+          float lat = t.readLatDeg();
+          float lon = t.readLonDeg();
+          uint32_t epoch = initialTime + ((millis() - offset) / 1000);
+          uint32_t ms = millis() % 1000;
+          String stamp = String::format("%lu%lu", epoch, ms);
+          int accel = t.readZ();
+          String data = String::format("{ \"La\": \"%f\", \"Lo\": \"%f\", \"T\": \"%s\", \"H\": \"%d\", \"id\": \"%d\" }", lat, lon, stamp.c_str(), accel, id);
+          myFile.open("TrailData.txt", O_RDWR | O_AT_END);
+          myFile.println(data);
+          myFile.close();
+        }
+      }
+      break;
+    case SEND_STATE:
+    Serial.println("In SEND_STATE");
+      t.gpsOff();
+      if(DEBUG == 1)
+      {
+        state = POWER_DOWN_STATE;
+        delay(1000);
+        Serial.println("Moving to POWER_DOWN_STATE");
+        break;
+      }
+      Cellular.on();
+      Cellular.connect();
+      while(!Cellular.ready());
+      Particle.connect();
+      myFile.open("TrailData.txt", O_RDWR);
+      char arr[256];
+      size_t n;
+      while((n = myFile.fgets(arr, sizeof(arr))) > 0)
+      {
+          String str(arr);
+          Serial.println(str);
+          //Particle.publish("Heat", str, PRIVATE);
+          //delay(1500);
+      }
+      myFile.close();
+      //remove("TrailData.txt");
+      state = POWER_DOWN_STATE;
+      break;
+    case POWER_DOWN_STATE:
+      Serial.println("In POWER_DOWN_STATE");
+      delay(1000);
+      System.sleep(D1, RISING);
+      delay(1000);
+      state = WAKE_STATE;
+      Serial.println("Moving to WAKE_STATE");
+      //Wake up on triggered pin, will have a voltage running from
+      //voltage regulator to a pin on the electron. Will wake up when
+      //vehicle is turned on
+      break;
+  }
+}
+
+void sendStateChange()
+{
+    state = SEND_STATE;
+    Serial.println("Moving to SEND_STATE");
+}
