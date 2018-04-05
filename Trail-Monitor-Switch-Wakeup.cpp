265a22b14ac1b8df0fe005691aadd2d1366f4e14
Daniel <36209256+kell1426@users.noreply.github.com>
2018-04-05 16:10:20 -0500

    Changed timestamp to gps based timestamp with gpsTime() function. Modified portion to create and deal with TrailDataExtras. Also added in a basic rms function for the harshness.

--- Trail-Monitor-Switch-Wakeup.cpp
+++ Trail-Monitor-Switch-Wakeup.cpp
@@ -4,6 +4,8 @@
 // This #include statement was automatically added by the Particle IDE.
 #include <SdFat.h>
 
+#include <math.h>
+#include <time.h>
 // Pick an SPI configuration.
 // See SPI configuration section below (comments are for photon).
 #define SPI_CONFIGURATION 0
@@ -17,21 +19,30 @@ const uint8_t chipSelect = D5;
 #define DEBUG 0
 int WKPIN = D1;
 SYSTEM_MODE(SEMI_AUTOMATIC);
-int id = 100;
 File myFile;
 File exFile;
 File IMUfile;
 File SpeedFile;
 AssetTracker t = AssetTracker();
 long lastGPSpoint = 0;
-uint32_t initialTime = 0;
-uint32_t offset = 0;
+long lastACCpoint = 0;
+int numAcc = 0;
+int numACCpoints = 0;
+float curSumSQ = 0;
+float sumsq = 0;
+float avgSumSQ = 0;
+int mag;
+int skip = 0;
+int alive = 0;
+//uint32_t initialTime = 0;
+//uint32_t offset = 0;
 enum State { WAKE_STATE, ACQUIRE_STATE, SEND_STATE, POWER_DOWN_STATE};
 State state = WAKE_STATE;
 int pin;
 
 void sendStateChange();
-String ms_last3digit_grabber(int ms)
+String ms_last3digit_grabber(int ms);
+uint32_t gpsTIme();
 
 void setup()
 {
@@ -93,17 +104,17 @@ void loop()
         delay(500);
         break;
       }
-      Cellular.on();
-      Cellular.connect();
-      while(!Cellular.ready());
-      Particle.connect();
-      Particle.syncTime();
-      delay(10000);
-      initialTime = Time.local();
-      offset = millis();
-      Particle.disconnect();
-      Cellular.disconnect();
-      Cellular.off();
+      // Cellular.on();
+      // Cellular.connect();
+      // while(!Cellular.ready());
+      // Particle.connect();
+      // Particle.syncTime();
+      // delay(10000);
+      // initialTime = Time.local();
+      // offset = millis();
+      // Particle.disconnect();
+      // Cellular.disconnect();
+      // Cellular.off();
       state = ACQUIRE_STATE;
       break;
     case ACQUIRE_STATE:
@@ -116,12 +127,20 @@ void loop()
       if ((millis()-lastGPSpoint) > (1000))
       {
         lastGPSpoint = millis();
+        numAcc = numACCpoints;
+        numACCpoints = 0;
+        curSumSQ = sumsq;
+        sumsq = 0;
+        avgSumSQ = 0;
+        if (numAcc>0) {
+          avgSumSQ = curSumSQ/numAcc;
+        }
+
         if(t.gpsFix())
         {
           int x;
           int y;
           int z;
-          int mag;
           float speed;
           speed = t.getSpeed();
           if(speed < 2.0) //Vehicle not in motion
@@ -137,12 +156,14 @@ void loop()
           }
           float lat = t.readLatDeg();
           float lon = t.readLonDeg();
-          uint32_t epoch = initialTime + ((millis() - offset) / 1000);
-          uint32_t ms = millis() % 1000;
+          //uint32_t epoch = initialTime + ((millis() - offset) / 1000);
+          uint32_t epoch = gpsTime();
+          //uint32_t ms = millis() % 1000;
+          uint32_t ms = t.getMilliseconds();
           String ms_string = ms_last3digit_grabber(ms);
           String stamp = String::format("%lu%s", epoch, ms_string.c_str());
-          int accel = t.readZ();
-          String data = String::format("{ \"La\": %f, \"Lo\": %f, \"T\": \"%s\", \"H\": %d, \"id\": %d }", lat, lon, stamp.c_str(), accel, id);
+          int accel = sqrt(avgSumSQ);
+          String data = String::format("{ \"La\": %f, \"Lo\": %f, \"T\": \"%s\", \"H\": %d }", lat, lon, stamp.c_str(), accel);
           myFile.open("TrailData.txt", O_RDWR | O_AT_END);
           myFile.println(data);
           myFile.close();
@@ -158,6 +179,13 @@ void loop()
             SpeedFile.close();
           }
         }
+      }
+      if ((millis()-lastACCpoint) > (10)) // Accelerometer every 10ms
+      {
+        lastACCpoint = millis();
+        mag = t.readXYZmagnitude();
+        numACCpoints++;
+        sumsq += mag*mag;
       }
       break;
     case SEND_STATE:
@@ -178,19 +206,21 @@ void loop()
       myFile.open("TrailData.txt", O_RDWR);
       char arr[256];
       size_t n;
-      int skip = 0;
-      int alive = 0;
+      skip = 0;
+      alive = 0;
 
       // Read and publish TrailData.txt data
       while((n = myFile.fgets(arr, sizeof(arr))) > 0)
       {
-        if (digitalRead(WKPIN)) { //Check if Vehicle is turned back on
+        if (digitalRead(WKPIN) == HIGH) { //Check if Vehicle is turned back on
           delay(1000); // Wait and make sure
-          if (digitalRead(WKPIN)) {
-            exFile.open("TrailDataExtras.txt", O_RDWR | O_AT_END);
-            exFile.println(str(arr));
-            while ((n = myFile.fgets(arr, sizeof(arr))) > 0) {
-              exFile.println(str(arr));
+          if (digitalRead(WKPIN) == HIGH) {
+            exFile.open("TrailDataExtras.txt", O_RDWR | O_AT_END | O_CREAT);
+            String str(arr);
+            exFile.println(str);
+            while ((n = myFile.fgets(arr, sizeof(arr))) > 0) { // Move lines from TrailData to Extras
+              String str(arr);
+              exFile.println(str);
             }
             skip = 1;
             alive = 1;
@@ -205,16 +235,18 @@ void loop()
 
       // Read and publish TrailDataExtras.txt data
       if (skip == 0) {
-        exFile.open("TrailDataExtras.txt", O_RDWR);
+        exFile.open("TrailDataExtras.txt", O_RDWR | O_CREAT);
         while((n = exFile.fgets(arr, sizeof(arr))) > 0)
         {
-          if (digitalRead(WKPIN)) { //Check if Vehicle is turned back on
+          if (digitalRead(WKPIN) == HIGH) { //Check if Vehicle is turned back on
             delay(1000); // Wait and make sure
-            if (digitalRead(WKPIN)) {
-              myFile.open("TrailData.txt", O_RDWR | O_AT_END);
-              myFile.println(str(arr));
-              while ((n = exFile.fgets(arr, sizeof(arr))) > 0) {
-                myFile.println(str(arr));
+            if (digitalRead(WKPIN) == HIGH) {
+              myFile.open("TrailData.txt", O_RDWR | O_AT_END | O_CREAT);
+              String str(arr);
+              myFile.println(str);
+              while ((n = exFile.fgets(arr, sizeof(arr))) > 0) { // Move lines from Extras to TrailData
+                String str(arr);
+                myFile.println(str);
               }
               alive = 1;
               break;
@@ -254,7 +286,10 @@ void loop()
 void sendStateChange()
 {
     state = SEND_STATE;
-    Serial.println("Moving to SEND_STATE");
+    if(DEBUG == 1)
+    {
+      Serial.println("Moving to SEND_STATE");
+    }
 }
 
 String ms_last3digit_grabber(int ms)
@@ -273,4 +308,23 @@ String ms_last3digit_grabber(int ms)
     digits = String::format("%d", ms);
   }
   return digits;
+}
+
+uint32_t gpsTime()
+{
+      struct tm T;
+      time_t t_of_day;
+
+      T.tm_year = t.getYear()-1900;
+      T.tm_mon = t.getMonth() - 1;     // Month, 0 - jan
+      T.tm_mday = t.getDay();          // Day of the month
+      T.tm_hour = t.getHour();
+      T.tm_min = t.getMinute();
+      T.tm_sec = t.getSeconds();
+      T.tm_isdst = 1;        // Is DST on? 1 = yes, 0 = no, -1 = unknown
+      t_of_day = mktime(&T);
+
+      //printf("seconds since the Epoch: %ld\n", (long) t_of_day);
+      uint32_t timestamp = (uint32_t)t_of_day;
+      return timestamp;
 }
